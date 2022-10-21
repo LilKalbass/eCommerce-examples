33100d2f1232da099eddd130a93b339b6aa5e1c4
Artem Dimoglov <adimoglov@gmail.com>
2022-10-21 04:56:16 +0300

    init

---
+++ src/components/home/pages/Pages.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Header} from '../../common/Header';
+import {Home} from '../Home';
+import {Footer} from '../../common/Footer';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
+export const Pages = () => {
+    return (
+        <>
+            <Router>
+                <Header/>
+                {/*    <Switch>*/}
+                {/*        <Route exact path= '/' component = {Home}/>*/}
+                {/*    </Switch>*/}
+                {/*<Footer/>*/}
+            </Router>
+        </>
+    );
+}
